c887f21ff0ec7a91dd24f4fd80e87473b93ddcd2
kamisho712 <syok712@gmail.com>
2022-09-20 00:53:48 +0900

    単体Usersモデルのテストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  VALID_PASSWORD_REGEX_2 = /\A[ぁ-んァ-ン一-龥]/
-  VALID_PASSWORD_REGEX_3 = /\A[ァ-ヶー－]+\z/
-  
-  validates :nickname,          presence: true
-  validates :family_name,       presence: true, format: { with: VALID_PASSWORD_REGEX_2, message: "is full-width characters"}
-  validates :first_name,        presence: true, format: { with: VALID_PASSWORD_REGEX_2, message: "is full-width characters"}
-  validates :family_name_kana,  presence: true, format: { with: VALID_PASSWORD_REGEX_3, message: "is full-width katakana"}
-  validates :first_name_kana,   presence: true, format: { with: VALID_PASSWORD_REGEX_3, message: "is full-width katakana"}
+  validates :nickname, presence: true
+  validates :password,                          format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :family_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :first_name,        presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :family_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :first_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
   validates :birthday,          presence: true
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "must contain letters and numbers"}
-
-
 end
