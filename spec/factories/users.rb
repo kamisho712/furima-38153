b7e7ee50ecceeafd71681db0060207afbd072e2a
kamisho712 <syok712@gmail.com>
2022-09-21 00:29:03 +0900

    Fakerで生成するパスワードを必ず英数字が含む形に修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     family_name           { gimei.family.kanji }
     first_name            { gimei.first.kanji }
