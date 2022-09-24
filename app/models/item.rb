88f2473ebb56501c93ef8af238e7004c9b0b90aa
kamisho712 <syok712@gmail.com>
2022-09-24 21:47:14 +0900

    items,userモデルのバリデーションとアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence:true
+  validates :description, presence:true
+  validates :category_id, presence:true
+  validates :item_condition_id, presence:true
+  validates :delivery_charge_id, presence:true
+  validates :prefecture_id, presence:true
+  validates :delivery_day_id, presence:true
+  validates :price, presence:true
 end
