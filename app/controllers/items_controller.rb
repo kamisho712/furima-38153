8aa3b1e47a7ce14011bc09a9c2a9e25e71cc82d4
kamisho712 <syok712@gmail.com>
2022-09-24 21:46:37 +0900

    itemsコントローラー　createアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,16 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :user_id )
+  end
+
+
 end
