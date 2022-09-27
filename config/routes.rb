adbbaa3022d6a233ed8c70c84983cb4f0aea02b5
kamisho712 <syok712@gmail.com>
2022-09-28 01:36:34 +0900

    ログアウトできないエラーの解決

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create]
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
