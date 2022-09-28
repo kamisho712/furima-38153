4b6ecc7d58020414a16fa75ea1f698d5917eec6a
kamisho712 <syok712@gmail.com>
2022-09-28 21:57:19 +0900

    ルーティングへの不要な記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create]
-
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
 end
