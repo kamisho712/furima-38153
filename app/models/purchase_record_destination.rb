d63bdf8f80345ad708a7e6f0cc3be26fa406d3b0
kamisho712 <syok712@gmail.com>
2022-10-28 02:43:07 +0900

    クレジットカード決済機能の実装

--- app/models/purchase_record_destination.rb
+++ app/models/purchase_record_destination.rb
@@ -1,6 +1,6 @@
 class PurchaseRecordDestination
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_record_id
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
   
   with_options presence: true do
     validates :user_id
@@ -10,11 +10,11 @@ class PurchaseRecordDestination
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A0\d{9,10}\z/, allow_blank: true, message: "must be at least 10 and no more than 11 single-byte digits" }, length: { minimum: 10, maximum: 11, allow_blank: true }
-    validates :purchase_record_id
+    validates :token
   end
 
   def save
-    @purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
-    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, purchase_record_id: purchase_record.id)
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
